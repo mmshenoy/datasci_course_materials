097b3e8c53d8c4b5ba7253712c98d5930663814b
mmshenoy <msmohit004@gmail.com>
2013-06-07 19:00:06 -0700

    assignment 2 files

---
+++ assignment2/sql/file4.sql
@@ -0,0 +1,3 @@
+select count(docid)
+from frequency
+where term = "parliament";
---
+++ assignment2/sql/file5.sql
@@ -0,0 +1,8 @@
+select count(*)
+from
+(
+select distinct(docid)
+from frequency
+group by docid
+having sum(count) > 300
+);
